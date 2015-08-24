d486061f06a4ee606805a4fb5f1bb1bc9483ccf3
Philip McCullick <pcmccull@gmail.com>
2015-08-24 13:41:32 -0700

    mostly working

--- tasks/build/sprites.js
+++ tasks/build/sprites.js
@@ -4,13 +4,13 @@ var imagemin = require("gulp-imagemin");
 var texturepacker = require("spritesmith-texturepacker");
 
 gulp.task("build:sprites", function() {
-    var spriteData = gulp.src("src/sprites/*.png")
-        .pipe(spritesmith({
-            imgName: "sprites.png",
-            cssName: "sprites.json",
-            algorithm: "binary-tree",
-            cssTemplate: texturepacker
-    }));
-    spriteData.img.pipe(imagemin()).pipe(gulp.dest("server/assets/"));
-    spriteData.css.pipe(gulp.dest("server/assets"));
+  var spriteData = gulp.src("src/sprites/**/*.png")
+      .pipe(spritesmith({
+          imgName: "sprites.png",
+          cssName: "sprites.json",
+          algorithm: "binary-tree",
+          cssTemplate: texturepacker
+  }));
+  spriteData.img.pipe(imagemin()).pipe(gulp.dest("server/assets/"));
+  spriteData.css.pipe(gulp.dest("server/assets"));
 });
